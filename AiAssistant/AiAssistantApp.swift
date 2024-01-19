e3e3183755cdb4044222e12a1c32b41ad50d59df
reassign <snowboardz125@gmail.com>
2024-01-19 11:32:08 -0500

    Initial Commit

---
+++ AiAssistant/AiAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiAssistantApp.swift
+//  AiAssistant
+//
+//  Created by Alex Mierzejewski on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AiAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
